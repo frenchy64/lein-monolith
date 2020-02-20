13f762225e29225ba4337d07587d5e55a4eed2d9
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-02-19 19:56:37 -0500

    release threatgrid/lein-monolith "1.4.1-SNAPSHOT-classifier-cycle-0"

--- project.clj
+++ project.clj
@@ -2,14 +2,14 @@
 ; everything before -classifier-cycle are the same as the upstream library.
 ; y in classifier-cycle-y should be incremented every time we release a new
 ; version based on the same upstream dep.
-(defproject threatgrid/lein-monolith "1.4.1-SNAPSHOT-classifier-cycle-0-SNAPSHOT"
+(defproject threatgrid/lein-monolith "1.4.1-SNAPSHOT-classifier-cycle-0"
   :description "Leiningen plugin for managing subrojects within a monorepo."
   :url "https://github.com/amperity/lein-monolith"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :eval-in-leiningen true
-  :deploy-branches ["master"]
+  :deploy-branches ["classifier-cycle"]
 
   :dependencies
   [[manifold "0.1.8"]
